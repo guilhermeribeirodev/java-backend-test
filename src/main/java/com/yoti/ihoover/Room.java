abf432a63a441f1239dcd28108a966d4cf02d6eb
guilhermeribeiro <guilhermeribeirodev@gmail.com>
2018-02-17 13:12:26 +0000

    implemented scenario for room creation

---
+++ src/main/java/com/yoti/ihoover/Room.java
@@ -0,0 +1,23 @@
+package com.yoti.ihoover;
+
+public final class Room {
+
+    private int x;
+    private int y;
+
+    private Room(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static Room createRoom(int x, int y) {
+
+        return new Room(x,y);
+    }
+
+    Room(){}
+
+    public int numberOfPositions() {
+        return x*y;
+    }
+}
--- src/test/java/com/yoti/ihoover/RoomSteps.java
+++ src/test/java/com/yoti/ihoover/RoomSteps.java
@@ -5,12 +5,18 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+
 public class RoomSteps {
+
+    Room room;
 
     @Given("^a room with the following dimensions X = (\\d+) and Y = (\\d+)$")
     public void a_room_with_the_following_dimensions_X_and_Y(int arg1, int arg2) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        room = Room.createRoom(2,2);
+        assertThat(room.numberOfPositions(), is(4));
     }
 
     @When("^user asks for numberOfPositions of room$")
