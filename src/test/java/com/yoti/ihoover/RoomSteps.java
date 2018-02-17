3a042696ac417e1062314454d2ece44c37f4146b
guilhermeribeiro <guilhermeribeirodev@gmail.com>
2018-02-17 13:20:29 +0000

    test refactor

--- src/test/java/com/yoti/ihoover/RoomSteps.java
+++ src/test/java/com/yoti/ihoover/RoomSteps.java
@@ -12,13 +12,13 @@ public class RoomSteps {
     Room room;
 
     @Given("^a room with the following dimensions X = (\\d+) and Y = (\\d+)$")
-    public void a_room_with_the_following_dimensions_X_and_Y(int x, int y) throws Throwable {
+    public void a_room_with_the_following_dimensions_X_and_Y(int x, int y)  {
         room = Room.createRoom(x,y);
 
     }
 
-    @Then("^result numberOfPositions of room must be (\\d+)$")
-    public void result_must_be(int positions) throws Throwable {
+    @Then("^numberOfPositions of room must be (\\d+)$")
+    public void result_must_be(int positions)  {
         assertThat(room.numberOfPositions(), is(positions));
     }
 }
--- src/test/resources/com/yoti/ihoover/room.feature
+++ src/test/resources/com/yoti/ihoover/room.feature
@@ -2,4 +2,4 @@ Feature: Room specification
   Scenario: Ensure room is created
 
     Given a room with the following dimensions X = 2 and Y = 2
-    Then result numberOfPositions of room must be 4
+    Then numberOfPositions of room must be 4
